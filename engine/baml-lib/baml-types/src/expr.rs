1204b1d8283fc7575a5aaae62c0abab8bc285d88
Greg Hale <imalsogreg@gmail.com>
2025-02-27 12:03:50 -0800

    mini-wip

--- engine/baml-lib/baml-types/src/expr.rs
+++ engine/baml-lib/baml-types/src/expr.rs
@@ -1,16 +1,42 @@
-#[macro_use]
-extern crate moniker;
-
-// use moniker::{Embed, Binder, Rec, Scope, Var};
-use moniker::{Binder, Scope, Var};
+use moniker::{Binder, BoundTerm, Scope, Var};
 use std::sync::Arc;
 
 use crate::BamlValueWithMeta;
 
+type Name = String;
+
+/// A BAML expression term.
+/// T is the type of the BamlValue metadata.
+/// U is the type of arbitrary metadata, for example types.
 #[derive(Debug, Clone, BoundTerm)]
 pub enum Expr<T,U> {
     Atom(BamlValueWithMeta<U>,T),
-    Var(Var<String>),
-    Lambda(Scope<Binder<String>, Arc<Expr<T,U>>>),
+    Var(Var<Name>, T),
+    Lambda(Scope<Vec<Binder<Name>>, Arc<Expr<T,U>>>, T),
     App(Arc<Expr<T,U>>, Arc<Expr<T,U>>, T)
+}
+
+
+
+pub fn step<T,U>(
+    context: &Scope<Binder<Vec<Name>>, Arc<Expr<T,U>>>,
+    expr: &mut Expr<T,U>
+) -> anyhow::Result<Expr<T,U>>
+where
+    T: Clone + std::fmt::Debug,
+    U: Clone + std::fmt::Debug,
+{
+    match expr {
+        Expr::Var(v, t) => {
+            if let Some(e) = context.get(v) {
+                Ok(e.clone())
+            } else {
+                Err(anyhow::anyhow!("Variable not found: {}", v))
+            }
+        }
+        Expr::App(f, x, t) => {
+            Some(Expr::App(f.clone(), x.clone(), t.clone()))
+        }
+        _ => None
+    }
 }
